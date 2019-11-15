32a1fc9988309f750d5214fe9e190958571a5897
yash <sahani.yash@gmail.com>
2019-11-15 14:49:39 -0800

    adding ModelViewAdapter

---
+++ ProjectGo/src/main/java/go/adapter/ModelViewAdapter.java
@@ -0,0 +1,14 @@
+package go.adapter;
+
+import java.awt.Point;
+import go.model.observer.*;
+import go.view.observer.*;
+
+
+
+
+public class ModelViewAdapter implements GoGameObserver, GoMoveObserver, GoViewSubject {
+
+
+
+}
