e85d10bd16274bd220f5d60542f71d2a227ed403
parvathysjsu <parvathy.kannankumarathmadomkrishnan@sjsu.edu>
2019-11-14 17:21:13 -0800

    capture 2

--- ProjectGo/src/main/java/go/model/gameplay/capturing/GoCaptureImpl.java
+++ ProjectGo/src/main/java/go/model/gameplay/capturing/GoCaptureImpl.java
@@ -10,7 +10,9 @@ import go.model.datamodel.impl.GoMoveImpl;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class GoCaptureImpl implements GoCapture {
     int SIZE;
@@ -24,54 +26,104 @@ public class GoCaptureImpl implements GoCapture {
         int row = move.getPoint().getX();
         int col = move.getPoint().getY();
         // get neighbors
-        List<GoMoveImpl> neighbors = new ArrayList<GoMoveImpl>();        
-        if (row > 0) {
-            GoPoint location = new GoPointImpl(row - 1, col);
-            Optional<StoneColor> stone = board.getStone(location);
-            if(stone.isPresent()){
-                neighbors.add(new GoMoveImpl(location, stone.get()));    
-            }                    
-        }
-        if (row < SIZE - 1) {
-            GoPoint location = new GoPointImpl(row + 1, col);
-            Optional<StoneColor> stone = board.getStone(location);
-            if(stone.isPresent()){
-                neighbors.add(new GoMoveImpl(location, stone.get()));    
-            } 
-        }
-        if (col > 1) {
-            GoPoint location = new GoPointImpl(row, col - 1);
-            Optional<StoneColor> stone = board.getStone(location);
-            if(stone.isPresent()){
-                neighbors.add(new GoMoveImpl(location, stone.get()));    
-            }             
-        }
-        if (col < SIZE - 1) {
-            GoPoint location = new GoPointImpl(row, col + 1);
-            Optional<StoneColor> stone = board.getStone(location);
-            if(stone.isPresent()){
-                neighbors.add(new GoMoveImpl(location, stone.get()));    
-            }             
-        }
-
+        List<GoMoveImpl> neighbors = getNeighbours(row, col, board);
         for (GoMoveImpl neighbor : neighbors) {
             if (neighbor.getStoneColor() != move.getStoneColor()) {
-                checkforCapture(neighbor, capturedPoints);
-                continue;
+                //todo  
+                chainLiberties(neighbor, board, capturedPoints);                              
             }
         }
-
         return capturedPoints.size() == 0? Collections.emptyList() : capturedPoints;
     }
 
-/**
- * Check for capturing Stone
- * @param neighbor
- * @param capturedPoints
- */
-private void checkforCapture(GoMoveImpl neighbor, List<GoPoint> capturedPoints) {
-    //find chain of neighbour and get its liberties
-    //TO-DO
+private void chainLiberties(GoMoveImpl stone, GoGameBoard board, List<GoPoint> capturedPoints) {
+    boolean chainHasNoliberties = true;
+    //when chain has no liberties, return true
+    Set<GoMoveImpl> chain = findChain( stone, board);
+    for (GoMoveImpl e : chain) {
+        if(getNeighbours(e.getPoint().getX(), e.getPoint().getY(), board).size() != 4)
+        chainHasNoliberties = false;
+    }
+    if(chainHasNoliberties){
+        for (GoMoveImpl e : chain) {
+            capturedPoints.add(e.getPoint());
+        }
+    }
 }
 
+private Set<GoMoveImpl> findChain(GoMoveImpl stone, GoGameBoard board) {
+    int row = stone.getPoint().getX();
+    int col = stone.getPoint().getY();
+    StoneColor color = stone.getStoneColor();
+    Set<GoMoveImpl> elements = new HashSet<GoMoveImpl>(); 
+    Set<GoMoveImpl> currentElements = new HashSet<GoMoveImpl>(); 
+    currentElements.add(stone);
+    Set<GoMoveImpl> prevElements;
+    boolean allPresent = false;
+    while (!allPresent) {
+        allPresent = false;
+        boolean flag = true;
+        for (GoMoveImpl e : currentElements) {
+            if(!elements.contains(e))
+                flag = false;
+        }
+        if(flag)
+            allPresent = true;
+        for (GoMoveImpl e : currentElements) {
+           elements.add(e);
+        }
+        prevElements = currentElements;
+        currentElements = new HashSet<GoMoveImpl>(); 
+        for (GoMoveImpl e : prevElements) {
+            List<GoMoveImpl> neighbors = getSameColorNeighbours(e.getPoint().getX(), e.getPoint().getY(), board, e.getStoneColor());
+            for (GoMoveImpl a : neighbors) {
+                currentElements.add(a);
+            }
+         }        
+    }
+    return elements;
+}
+
+private  List<GoMoveImpl> getNeighbours(int row, int col, GoGameBoard board) {
+    List<GoMoveImpl> neighbors = new ArrayList<GoMoveImpl>(); 
+    if (row > 0) {
+        GoPoint location = new GoPointImpl(row - 1, col);
+        Optional<StoneColor> stone = board.getStone(location);
+        if(stone.isPresent()){
+            neighbors.add(new GoMoveImpl(location, stone.get()));    
+        }                    
+    }
+    if (row < SIZE - 1) {
+        GoPoint location = new GoPointImpl(row + 1, col);
+        Optional<StoneColor> stone = board.getStone(location);
+        if(stone.isPresent()){
+            neighbors.add(new GoMoveImpl(location, stone.get()));    
+        } 
+    }
+    if (col > 1) {
+        GoPoint location = new GoPointImpl(row, col - 1);
+        Optional<StoneColor> stone = board.getStone(location);
+        if(stone.isPresent()){
+            neighbors.add(new GoMoveImpl(location, stone.get()));    
+        }             
+    }
+    if (col < SIZE - 1) {
+        GoPoint location = new GoPointImpl(row, col + 1);
+        Optional<StoneColor> stone = board.getStone(location);
+        if(stone.isPresent()){
+            neighbors.add(new GoMoveImpl(location, stone.get()));    
+        }             
+    }
+    return neighbors;
+}
+
+private  List<GoMoveImpl> getSameColorNeighbours(int row, int col, GoGameBoard board, StoneColor color) {
+    List<GoMoveImpl> neighbors = getNeighbours(row, col, board);
+    for (GoMoveImpl neighbor : neighbors) {
+        if (neighbor.getStoneColor() != color) {
+            neighbors.remove(neighbor);          
+        }
+    }
+    return neighbors;
+}
 }
