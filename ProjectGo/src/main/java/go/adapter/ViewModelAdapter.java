caa89d515c78774bb8eea675f1eb364df240e299
yash <sahani.yash@gmail.com>
2019-11-15 15:02:15 -0800

    redid changes

--- ProjectGo/src/main/java/go/adapter/ViewModelAdapter.java
+++ ProjectGo/src/main/java/go/adapter/ViewModelAdapter.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package go.adapter;
 
 import java.awt.Point;
@@ -71,77 +71,3 @@ public class ViewModelAdapter implements GoViewObserver, GoGameSubject {
     }
 
 }
-=======
-package go.adapter;
-
-import java.awt.Point;
-
-import go.controller.GoMoveController;
-import go.model.datamodel.GoMove;
-import go.model.datamodel.GoPoint;
-import go.model.datamodel.StoneColor;
-import go.model.observer.GoGameObserver;
-import go.model.observer.GoGameSubject;
-import go.model.observer.GoMoveObserver;
-import go.view.observer.GoViewObserver;
-
-public class ViewModelAdapter implements GoViewObserver, GoGameSubject {
-
-	private GoMoveController goMoveController;
-	
-	@Override
-	public void handleMouseClickEvent(Point point) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void handlePassTurnRequest() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void handleUndoMoveRequest() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void handleWindowClose() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void addMoveObserver(GoMoveObserver observer) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void addGameObserver(GoGameObserver observer) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void notifyObserversOfPiecePlacement(GoMove move) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void notifyObserversOfPieceRemoval(GoPoint point) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void notifyObserversOfGameEnd(StoneColor winner) {
-		// TODO Auto-generated method stub
-		
-	}
-
-}
->>>>>>> a31fc917ae0b4efea9fe62392b4e969006625f3d
