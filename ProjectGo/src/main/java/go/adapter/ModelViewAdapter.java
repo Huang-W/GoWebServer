c1fdf422ed960bf08adfd9dcd20b47cc5a9ef97c
yash <sahani.yash@gmail.com>
2019-11-15 15:00:36 -0800

    more changes

--- ProjectGo/src/main/java/go/adapter/ModelViewAdapter.java
+++ ProjectGo/src/main/java/go/adapter/ModelViewAdapter.java
@@ -1,14 +1,65 @@
 package go.adapter;
 
 import java.awt.Point;
+
+import go.controller.GoViewController;
+import go.model.datamodel.GoMove;
+import go.model.datamodel.GoPoint;
+import go.model.datamodel.StoneColor;
 import go.model.observer.*;
-import go.view.observer.*;
-
-
-
+import go.view.observer.GoViewObserver;
+import go.view.observer.GoViewSubject;
 
 public class ModelViewAdapter implements GoGameObserver, GoMoveObserver, GoViewSubject {
 
+    private GoViewController goViewController;
 
+    @Override
+    public void handleGameEnd(StoneColor winner) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void handlePieceAdditionEvent(GoMove move) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void handlePieceRemovalEvent(GoPoint point) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void addViewObserver(GoViewObserver observer) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void notifyObserversOfMouseClick(Point point) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void notifyObserversOfPassTurnRequest() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void notifyObserversOfUndoMoveRequest() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void notifyObserversOfWindowClose() {
+        // TODO Auto-generated method stub
+
+    }
 
 }
---
+++ ProjectGo/src/main/java/go/adapter/ViewModelAdapter.java
@@ -0,0 +1,72 @@
+package go.adapter;
+
+import java.awt.Point;
+
+import go.controller.GoMoveController;
+import go.model.datamodel.GoMove;
+import go.model.datamodel.GoPoint;
+import go.model.datamodel.StoneColor;
+import go.model.observer.GoGameObserver;
+import go.model.observer.GoGameSubject;
+import go.model.observer.GoMoveObserver;
+import go.view.observer.GoViewObserver;
+
+public class ViewModelAdapter implements GoViewObserver, GoGameSubject {
+
+    private GoMoveController goMoveController;
+
+    @Override
+    public void handleMouseClickEvent(Point point) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void handlePassTurnRequest() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void handleUndoMoveRequest() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void handleWindowClose() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void addMoveObserver(GoMoveObserver observer) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void addGameObserver(GoGameObserver observer) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void notifyObserversOfPiecePlacement(GoMove move) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void notifyObserversOfPieceRemoval(GoPoint point) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void notifyObserversOfGameEnd(StoneColor winner) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
